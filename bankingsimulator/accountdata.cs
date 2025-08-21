6bb6bfdc31aacce3db9410ae70ae99132e7e654d
LukeFaure <127924225+LukeFaure@users.noreply.github.com>
2025-08-21 05:28:07 +0000

    worked on new banking simulator

---
+++ bankingsimulator/accountdata.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Linq;
+namespace AccountData
+{
+    public class AccountRecords
+    {
+        public string Name { get; set; }
+        public int Pin { get; set;  }
+        public int Bsb { get; set; }
+        public AccountRecords(string name, int pin, int bsb)
+        {
+            if (Program.accounts.Any(a => 
+                a[0].Equals(name) ||
+                a[1].Equals(pin) ||
+                a[2].Equals(bsb)))
+                throw new ArgumentException("Error 001; one or more of the values already exist, please try again");
+
+            Name = name;
+            Pin = pin;
+            Bsb = bsb;
+
+            Program.accountNames.Add(name);
+            Program.accountPins.Add(pin);
+            Program.accountBsbs.Add(bsb);
+            Program.accounts.Add(new object[] { name, pin, bsb });
+        }
+    } 
+}
---
+++ bankingsimulator/accountmanagement.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AccountManagement
+{
+    public static class AccountManaging
+    { 
+        public static void CreateAccount(string accountName, int accountPin)
+        {
+            
+        }
+        public static void RemoveAccount(string accountName, int accountPin)
+        {
+
+        }   
+        public static void EditAccount(string accountName, int accountPin, string newAccountName, int newAccountPin)
+        { 
+
+        }
+        
+    }
+}

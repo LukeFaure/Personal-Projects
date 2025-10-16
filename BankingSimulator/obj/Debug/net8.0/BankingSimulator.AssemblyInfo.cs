535cf8fa426710367702253b747a95ddb72f58e4
LukeFaure <127924225+LukeFaure@users.noreply.github.com>
2025-10-16 02:11:13 +0000

    editting

--- BankingSimulator/obj/Debug/net8.0/BankingSimulator.AssemblyInfo.cs
+++ BankingSimulator/obj/Debug/net8.0/BankingSimulator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankingSimulator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ebc571b85d259f15bd6a6b0a5a4c42412e63d20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dd726aef60cf451e51fb66f4dade67548c71c9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankingSimulator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankingSimulator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
